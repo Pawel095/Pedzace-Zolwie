1421e102b8b5cabca2c860183d5245225da251d0
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-10-12 19:39:17 +0200

    Controllers generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { MapViewComponent } from "./map-view/map-view.component";
+import { CardViewComponent } from "./card-view/card-view.component";
+import { ScoreboardComponent } from "./scoreboard/scoreboard.component";
+import { RulesComponent } from "./rules/rules.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MapViewComponent,
+    CardViewComponent,
+    ScoreboardComponent,
+    RulesComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
