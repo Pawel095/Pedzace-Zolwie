25e9f0f5f2ffabd3146742abd141b1745c5ed660
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-10-16 17:24:05 +0200

    Added link to debug page; rules creates example cards

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Card } from "../Models/Card";
+import { CardMarkings } from "../Enums/CardMarkings";
 
 @Component({
   selector: "app-rules",
@@ -6,7 +8,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./rules.component.scss"]
 })
 export class RulesComponent implements OnInit {
-  constructor() {}
+  C1: Card = new Card();
+  C2: Card = new Card();
+  L1: Card = new Card();
+  L2: Card = new Card();
+  C_1: Card = new Card();
+
+  constructor() {
+    this.C1.marking = CardMarkings.COLOUR_ONE_FORWARD;
+    this.C1.colour = Math.floor(Math.random() * 5);
+
+    this.C2.marking = CardMarkings.COLOUR_TWO_FORWARD;
+    this.C2.colour = Math.floor(Math.random() * 5);
+
+    this.L1.marking = CardMarkings.LAST_ONE_FORWARD;
+    this.L1.colour = Math.floor(Math.random() * 5);
+
+    this.L2.marking = CardMarkings.LAST_TWO_FORWARD;
+    this.L2.colour = Math.floor(Math.random() * 5);
+
+    this.C_1.marking = CardMarkings.COLOUR_ONE_BACK;
+    this.C_1.colour = Math.floor(Math.random() * 5);
+  }
 
   ngOnInit() {}
 }
