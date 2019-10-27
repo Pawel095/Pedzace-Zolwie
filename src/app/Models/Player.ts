d9d19feed6ef4ee2e2fe0e70bff94a36b5b020b7
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-10-27 12:46:32 +0100

    Added Randomized id to players

--- src/app/Models/Player.ts
+++ src/app/Models/Player.ts
@@ -3,10 +3,12 @@ import { TurtleColours } from "../Enums/TurtleColours";
 import { Card } from "./Card";
 
 export class Player {
+    readonly id: number;
     playerType: PlayerTypes;
     hand: Array<Card>;
     turtleColour: TurtleColours;
     constructor(type: PlayerTypes, colour: TurtleColours) {
+        this.id = Math.floor(Math.random() * 10000);
         this.playerType = type;
         this.turtleColour = colour;
     }
