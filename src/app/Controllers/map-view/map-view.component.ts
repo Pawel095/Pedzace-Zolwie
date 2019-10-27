de3e219b49f338c17c36eca9eedeee26c6c76c6f
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-10-27 12:54:05 +0100

    MainViewComponent displays tiles properly

--- src/app/Controllers/map-view/map-view.component.ts
+++ src/app/Controllers/map-view/map-view.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from "@angular/core";
+import {
+    Component,
+    ElementRef,
+    HostListener,
+    OnInit,
+    ViewChild
+} from "@angular/core";
 import { Point } from "src/app/Models/Point";
-import { mapToMapExpression } from "@angular/compiler/src/render3/util";
+import { GameStateService } from "src/app/Servces/game-state.service";
 
 @Component({
     selector: "app-map-view",
@@ -9,26 +15,20 @@ import { mapToMapExpression } from "@angular/compiler/src/render3/util";
 })
 export class MapViewComponent implements OnInit {
     TILE_NUMBER = 10;
-    constructor() {}
     ctx: CanvasRenderingContext2D;
+    points: Array<Point> = [];
+
+    constructor(private gss: GameStateService) {}
 
     @ViewChild("map", { static: true }) map: ElementRef<HTMLCanvasElement>;
 
     @HostListener("window:resize", ["$event"])
     onResize() {
-        this.ctx = this.map.nativeElement.getContext("2d");
+        this.points = [];
         this.ctx.canvas.width = window.innerWidth - 200;
         this.ctx.canvas.height = window.innerHeight - 400;
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        this.redraw();
-    }
 
-    ngOnInit() {
-        this.onResize();
-    }
-
-    redraw() {
-        let points: Array<Point> = [];
         // console.log(this.ctx);
         // poziome: y sinus; x stałe
         let temp = Math.PI / 2;
@@ -36,28 +36,47 @@ export class MapViewComponent implements OnInit {
         if (this.ctx.canvas.width >= this.ctx.canvas.height) {
             for (let i = 0; i < this.TILE_NUMBER; i++) {
                 temp += piece;
-                points.push(
+                this.points.push(
                     new Point(
                         (this.ctx.canvas.width / this.TILE_NUMBER) * i + 10,
-                        (Math.sin(temp) * this.ctx.canvas.height/ 5)  + this.ctx.canvas.height / 2
+                        (Math.sin(temp) * this.ctx.canvas.height) / 5 +
+                            this.ctx.canvas.height / 2
                     )
                 );
             }
         } else {
             for (let i = 0; i < this.TILE_NUMBER; i++) {
                 temp += piece;
-                points.push(
+                this.points.push(
                     new Point(
-                        (Math.sin(temp) * this.ctx.canvas.width/5) + this.ctx.canvas.width / 2,
+                        (Math.sin(temp) * this.ctx.canvas.width) / 5 +
+                            this.ctx.canvas.width / 2,
                         (this.ctx.canvas.height / this.TILE_NUMBER) * i + 10
                     )
                 );
             }
         }
-        // render
+        this.render();
+    }
+
+    ngOnInit() {
+        this.ctx = this.map.nativeElement.getContext("2d");
+        this.onResize();
+        this.gss.registerGameStateUpdate(() => {
+            console.log("CALLBACK!");
+        });
+    }
+
+    render() {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
         this.ctx.fillStyle = "green";
-        points.forEach(e => {
-            this.ctx.fillRect(e.x, e.y, 20, 20);
+        this.points.forEach(e => {
+            this.ctx.fillRect(
+                e.x,
+                e.y,
+                this.ctx.canvas.width / 15,
+                this.ctx.canvas.height / 15
+            );
         });
     }
 }
