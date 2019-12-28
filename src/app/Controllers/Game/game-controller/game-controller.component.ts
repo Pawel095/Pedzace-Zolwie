6e8b0046781a1bebc00573d0902b643da48fba58
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-12-28 13:58:02 +0100

    Hot Seat V1 added.

--- src/app/Controllers/Game/game-controller/game-controller.component.ts
+++ src/app/Controllers/Game/game-controller/game-controller.component.ts
@@ -13,6 +13,9 @@ import { GameStateService } from 'src/app/Servces/game-state.service';
 import { environment } from 'src/environments/environment';
 import { EndGameDialogComponent } from './end-game-dialog/end-game-dialog.component';
 import { SelectColorDialogComponent } from './select-color-dialog/select-color-dialog.component';
+import { PlayerInstrance } from 'src/app/Models/PlayerInstance';
+import { platform } from 'os';
+import { PlayerType2Path } from 'src/app/Utils/playerType2Path';
 
 @Component({
     selector: 'app-game-controller',
@@ -26,21 +29,44 @@ export class GameControllerComponent implements OnInit, IPlayer {
         private snackBar: MatSnackBar,
         private router: Router
     ) {}
-    player: Player;
+
+    vsAiPlayer: Player;
+    HotSeatPlayers: PlayerInstrance[];
+
+    currentDisplayPlayer: Player;
+
     debug = !environment.production;
     ngOnInit() {
+        this.currentDisplayPlayer = new Player(PlayerTypes.HUMAN, TurtleColours.RED);
         // if (!environment.production) {
         //     this.gss.setup(GameModes.AI);
         // }
         if (!environment.production) {
-            this.gss.setup(GameModes.HOT_SEAT, { hu: 3 });
+            this.gss.setup(GameModes.HOT_SEAT, { hu: 2 });
         }
 
         switch (this.gss.currentGamemode) {
             case GameModes.AI:
                 this.gss.registerPlayer(this, PlayerTypes.HUMAN);
+                this.currentDisplayPlayer = this.vsAiPlayer;
+                break;
+            case GameModes.HOT_SEAT:
+                this.HotSeatPlayers = [];
+                for (let i = 0; i < this.gss.huAmmount; i++) {
+                    const p = new PlayerInstrance(this.gss);
+                    this.gss.registerPlayer(p, PlayerTypes.HUMAN);
+                    this.HotSeatPlayers.push(p);
+                }
+                this.gss.currentTurn$.subscribe(data => {
+                    const playerInstalce = this.HotSeatPlayers.find(e => e.player.id === data);
+                    console.log(playerInstalce);
+                    if (playerInstalce !== undefined) {
+                        this.currentDisplayPlayer = playerInstalce.player;
+                    }
+                });
                 break;
         }
+
         this.gss.gameEndStatus$.subscribe(data => {
             console.log('Opening Dialog');
             this.dialog
@@ -53,11 +79,20 @@ export class GameControllerComponent implements OnInit, IPlayer {
     }
 
     init(p: Player): void {
-        this.player = p;
+        this.vsAiPlayer = p;
     }
 
     cardClicked(input: { card: Card; discard: boolean }) {
         console.log(input);
+        let player: Player;
+        switch (this.gss.currentGamemode) {
+            case GameModes.AI:
+                player = this.vsAiPlayer;
+                break;
+            case GameModes.HOT_SEAT:
+                player = this.currentDisplayPlayer;
+                break;
+        }
         if (!input.discard) {
             console.log('playing');
             const card = input.card;
@@ -65,8 +100,8 @@ export class GameControllerComponent implements OnInit, IPlayer {
                 const dialogRef = this.dialog.open(SelectColorDialogComponent);
                 dialogRef.afterClosed().subscribe(data => {
                     if (data !== undefined) {
-                        if (this.gss.validateMove(new Move(this.player.id, card, data))) {
-                            this.gss.playerMove(new Move(this.player.id, card, data));
+                        if (this.gss.validateMove(new Move(player.id, card, data))) {
+                            this.gss.playerMove(new Move(player.id, card, data));
                         } else {
                             this.snackBar.open('You cannot do that!', 'Ok', {
                                 duration: 3 * 1000,
@@ -77,15 +112,15 @@ export class GameControllerComponent implements OnInit, IPlayer {
                     }
                 });
             } else {
-                if (this.gss.validateMove(new Move(this.player.id, card, card.colour))) {
-                    this.gss.playerMove(new Move(this.player.id, card, card.colour));
+                if (this.gss.validateMove(new Move(player.id, card, card.colour))) {
+                    this.gss.playerMove(new Move(player.id, card, card.colour));
                 } else {
                     this.snackBar.open('You cannot do that!', 'Ok', { duration: 3 * 1000, verticalPosition: 'bottom' });
                 }
             }
         } else {
             console.log('discarding');
-            this.gss.playerMove(new Move(this.player.id, input.card, input.card.colour, true));
+            this.gss.playerMove(new Move(player.id, input.card, input.card.colour, true));
         }
     }
 }
